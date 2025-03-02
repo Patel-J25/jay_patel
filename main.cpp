9b0b016f9502a7bad3700b8ed4279954483221d0
Patel-J25 <164651370+Patel-J25@users.noreply.github.com>
2025-03-01 23:25:34 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
-#include<iostream>
+#include <iostream>
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
 using namespace std;
-int main(){
-cout<<"hello world"<<endl;
-return 0;
+
+
+
+int main() {
+    srand(time(0)); // Set a new seed based on the current time
+    int random=rand()%100+1;
+    cout<<random;
+    
+     cout<<"Please enter number between 1 o 100"<<endl;
+     int userInput;
+     cin>>userInput;
+     int count=10;
+     while(count!=0)
+     {
+        if(userInput<random)
+        {
+            cout<<"Your answer is too low"<<endl;
+            cin>>userInput;
+            count--;
+            
+        }
+        else if (userInput>random)
+        {
+            cout<<"Your answer is too high"<<endl;
+            cin>>userInput;
+            count--;
+        }
+        else if(userInput==random){
+            cout<<"Congarts you found the number "<<userInput<<endl;
+            break;
+        }
+        
+     }
+    return 0;
 }
+
+    
